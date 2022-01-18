7b8a485475f513abfe6a063e8c3d22894db6f736
ahmadnadeem123 <ahmadnadeempucit@gmail.com>
2022-01-18 19:28:17 +0500

    Menu added

--- app/src/main/java/com/example/tripapplication/BookPage.java
+++ app/src/main/java/com/example/tripapplication/BookPage.java
@@ -4,10 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class BookPage extends AppCompatActivity {
 
     Toolbar t1;
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu2, menu);
+        return true;
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.pract:
+//                Intent obj = new Intent(MainActivity.this, Practice.class);
+//                startActivity(obj);
+//                break;
+//
+//            case R.id.quiz:
+//                Random rand = new Random();
+//                int ran = rand.nextInt(4);
+//                Intent objj = new Intent(MainActivity.this, Quiz_1.class);
+//                objj.putExtra("value", (String.valueOf(ran)));
+//                startActivity(objj);
+//                break;
+//
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//        return false;
+//    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/tripapplication/MainActivity.java
+++ app/src/main/java/com/example/tripapplication/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,6 +13,34 @@ public class MainActivity extends AppCompatActivity {
 
     Button b1;
     Toolbar t1;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.pract:
+//                Intent obj = new Intent(MainActivity.this, Practice.class);
+//                startActivity(obj);
+//                break;
+//
+//            case R.id.quiz:
+//                Random rand = new Random();
+//                int ran = rand.nextInt(4);
+//                Intent objj = new Intent(MainActivity.this, Quiz_1.class);
+//                objj.putExtra("value", (String.valueOf(ran)));
+//                startActivity(objj);
+//                break;
+//
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//        return false;
+//    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
