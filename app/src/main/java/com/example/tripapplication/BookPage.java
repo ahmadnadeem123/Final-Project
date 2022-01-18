bf01e1940c61a6fb1ef9828e165a8ad2001ec16c
ahmadnadeem123 <ahmadnadeempucit@gmail.com>
2022-01-18 19:09:38 +0500

    App Bar Added

--- app/src/main/java/com/example/tripapplication/BookPage.java
+++ app/src/main/java/com/example/tripapplication/BookPage.java
@@ -1,14 +1,20 @@
 package com.example.tripapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class BookPage extends AppCompatActivity {
 
+    Toolbar t1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_page);
+
+        t1=findViewById(R.id.toolbar);
+        setSupportActionBar(t1);
     }
 }
--- app/src/main/java/com/example/tripapplication/MainActivity.java
+++ app/src/main/java/com/example/tripapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tripapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,12 +11,16 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button b1;
+    Toolbar t1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        t1=findViewById(R.id.toolbar);
+
+        setSupportActionBar(t1);
         b1=findViewById(R.id.button1);
 
         b1.setOnClickListener(new View.OnClickListener() {
