082e37792bfa6c095e1cd04d61eede172366fe9c
ahmadnadeem123 <ahmadnadeempucit@gmail.com>
2022-01-21 01:05:09 +0500

    Navigation Added

--- app/src/main/java/com/example/tripapplication/MainActivity.java
+++ app/src/main/java/com/example/tripapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.tripapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,7 +13,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+
+    ActionBarDrawerToggle toggle;
+
+
+
 
     Button b1;
     Toolbar t1;
@@ -54,6 +67,51 @@ public class MainActivity extends AppCompatActivity {
         t1=findViewById(R.id.toolbar);
 
         setSupportActionBar(t1);
+
+
+
+
+
+        navigationView=findViewById(R.id.nav_view);
+        drawerLayout=findViewById(R.id.drawer);
+
+        toggle=new ActionBarDrawerToggle(this,drawerLayout,t1,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+            {
+                switch (menuItem.getItemId()) {
+                    case R.id.book:
+                        Intent intent = new Intent(MainActivity.this, BookPage.class);
+                        startActivity(intent);
+                        //drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.contact:
+                        Intent intent2 = new Intent(MainActivity.this, Contact.class);
+                        startActivity(intent2);
+                        //drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.about:
+                        Intent intent3 = new Intent(MainActivity.this, About.class);
+                        startActivity(intent3);
+                        //drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+
+
+
+
+
+
+
         b1=findViewById(R.id.button1);
 
         b1.setOnClickListener(new View.OnClickListener() {
