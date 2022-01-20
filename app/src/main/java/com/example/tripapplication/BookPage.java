35812d5ffd9400706c7fd6a47c7bca235cd107b8
ahmadnadeem123 <ahmadnadeempucit@gmail.com>
2022-01-20 13:08:40 +0500

    Date Picker added

--- app/src/main/java/com/example/tripapplication/BookPage.java
+++ app/src/main/java/com/example/tripapplication/BookPage.java
@@ -4,14 +4,31 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class BookPage extends AppCompatActivity {
 
     Toolbar t1;
+
+    DatePickerDialog picker;
+    EditText eText;
+    TextView tvw;
+
+    DatePickerDialog picker1;
+    EditText eText1;
+
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu2, menu);
         return true;
@@ -51,5 +68,70 @@ public class BookPage extends AppCompatActivity {
 
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
+
+
+        tvw=(TextView)findViewById(R.id.textView5);
+        eText=(EditText) findViewById(R.id.editText1);
+        eText.setInputType(InputType.TYPE_NULL);
+        eText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(BookPage.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                eText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+//        btnGet=(Button)findViewById(R.id.button1);
+//        btnGet.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                tvw.setText("Selected Date: "+ eText.getText());
+//            }
+//        });
+
+
+
+        eText1=(EditText) findViewById(R.id.editText2);
+        eText1.setInputType(InputType.TYPE_NULL);
+        eText1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker1 = new DatePickerDialog(BookPage.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                eText1.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker1.show();
+            }
+        });
+
+
+
+
+
+
+
+
+
+
     }
 }
