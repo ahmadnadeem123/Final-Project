f3d6edfe5b7c2b921571170714093c541c351e66
devil7907 <92261949+devil7907@users.noreply.github.com>
2022-01-23 21:48:39 +0500

    User Profile Details Stored in FireBase FireStore

--- app/src/main/java/com/example/tripapplication/Signup.java
+++ app/src/main/java/com/example/tripapplication/Signup.java
@@ -2,10 +2,12 @@ package com.example.tripapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,16 +16,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Signup extends AppCompatActivity {
     EditText fName,mail,pass,ph;
+    Toolbar t2;
     Button register;
     TextView login;
     ProgressBar pbar;
     FirebaseAuth Auth;
+    FirebaseFirestore fstore;
+    String userid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,13 +49,15 @@ public class Signup extends AppCompatActivity {
         login=findViewById(R.id.createtext);
         pbar=findViewById(R.id.progressBar);
         Auth=FirebaseAuth.getInstance();
-
+        t2=findViewById(R.id.toolbar2);
+        fstore=FirebaseFirestore.getInstance();
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email=mail.getText().toString().trim();
                 String password=pass.getText().toString().trim();
-
+                String name= fName.getText().toString();
+                String phone=ph.getText().toString();
                 if(TextUtils.isEmpty(email))
                 {
                     mail.setError("Email is Required");
@@ -67,6 +81,24 @@ public class Signup extends AppCompatActivity {
                         if(task.isSuccessful())
                         {
                             Toast.makeText(Signup.this,"Sign Up Successfull !",Toast.LENGTH_SHORT).show();
+                            userid=Auth.getCurrentUser().getUid();
+                            DocumentReference documentReference=fstore.collection("users").document(userid);
+                            Map<String,Object> user= new HashMap<>();
+                            user.put("fname",name);
+                            user.put("mail",email);
+                            user.put("ph",phone);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Log.d("","onSuccess: user profile is created for"+userid);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d("","OnFailure"+e.toString());
+                                }
+                            });
+
                             Intent i=new Intent(Signup.this,Login.class);
                             startActivity(i);
                         }
