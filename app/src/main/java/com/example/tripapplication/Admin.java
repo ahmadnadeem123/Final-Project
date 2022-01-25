a3424f7f37e3ab1dace63597e5e767da416f6a04
devil7907 <92261949+devil7907@users.noreply.github.com>
2022-01-25 21:44:35 +0500

    Merge Check

--- app/src/main/java/com/example/tripapplication/Admin.java
+++ app/src/main/java/com/example/tripapplication/Admin.java
@@ -2,11 +2,13 @@ package com.example.tripapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class Admin extends AppCompatActivity {
     Toolbar t3;
+    RecyclerView myrview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +16,7 @@ public class Admin extends AppCompatActivity {
 
         t3=findViewById(R.id.toolbar4);
         setSupportActionBar(t3);
+
+
     }
 }
